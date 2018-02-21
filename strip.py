a1d802fbc2116c337cbd3439c1c67548567130a5
latajacysmok <maksymles@gmail.com>
2018-02-21 05:58:17 +0100

    comment updates
    add program name in the comment at the beginning of the program.

--- strip.py
+++ strip.py
@@ -1,4 +1,4 @@
-#A function that fetches a text string and executes the same sentence as the method named strip (). 
+#A function strip.py that fetches a text string and executes the same sentence as the method named strip (). 
 #If the function is not passed on to other arguments than the text string to be processed, 
 #then whitespace should be removed from its beginning and end. Otherwise, 
 #the characters passed in the second function call are to be removed from the text string.
